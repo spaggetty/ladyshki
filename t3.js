11cdf9408d5f86c5023da76b71cdb67ab87c4fc5
spaggetty <spaggetty@ya.ru>
2020-03-13 21:32:32 +0300

    wooop

--- t3.js
+++ t3.js
@@ -1,14 +1,14 @@
 ﻿//а что если земля не круглая?)
 
 const fs = require('fs');
-var gm = fs.readdirSync('./qest/'); // получаем список файлов в папке
+let gm = fs.readdirSync('./qest/'); // получаем список файлов в папке
 
 
-var coll=0;
-var arqe = [];
+let coll=0;
+let arqe = [];
 
 //мешаем все вопросы из папки и оставляем 5 вопросов
-for (var i = 0; i < gm.length; i++)
+for (let i = 0; i < gm.length; i++)
   arqe.push(i);
 shuffl(arqe);
 
@@ -17,13 +17,13 @@ arqe = arqe.slice(0,5);
 
 
 
-for (var b = 0; b < 5; b++)
+for (let b = 0; b < 5; b++)
 {
-var array = fs.readFileSync("./qest/" + arqe[b] + ".txt" , "utf8").toString().split("\n");
+let array = fs.readFileSync("./qest/" + arqe[b] + ".txt" , "utf8").toString().split("\n");
 
 console.log('текст вопроса',array[0]);
 
-var verques = array[1];
+let verques = array[1];
 
 verques= verques.trim();
 console.log(verques);
@@ -54,7 +54,7 @@ console.log('количество правильных ответов: ', coll);
 
 //рандом с неповторяющимися числами
 function shuffl(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
+  let currentIndex = array.length, temporaryValue, randomIndex ;
 
 
   while (0 !== currentIndex) {
